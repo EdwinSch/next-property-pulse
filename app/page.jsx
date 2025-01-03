debbb5ee71773abb28ce54c31360a9b08f8bd5bf
Edwin <edwinschutjes@gmail.com>
2025-01-03 12:23:57 +0100

    add featured properties

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/Hero";
 import InfoBoxesLayout from "@/components/InfoBoxesLayout";
 import HomeProperties from "@/components/HomeProperties";
+import FeaturedProperties from "@/components/FeaturedProperties";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
       <InfoBoxesLayout />
+      <FeaturedProperties />
       <HomeProperties />
     </>
   );
